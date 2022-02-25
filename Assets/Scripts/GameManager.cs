5cf046c125b9913f00845abcbb61c72c1e05316a
RYUKI <ryukiarashi@icloud.com>
2022-02-25 10:14:20 +0900

    マップ上のインクを消す

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    /// <summary>インクの配列</summary>
+    GameObject[] _inkObjects;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        InkReset();
+    }
+
+    /// <summary>
+    /// マップにあるインク全てを消す
+    /// </summary>
+    void InkReset()
+    {
+        _inkObjects = GameObject.FindGameObjectsWithTag("Ink");
+
+        if (Input.GetButtonDown("Reset"))
+        {
+            for (int i = 0; i < _inkObjects.Length; i++)
+            {
+                Destroy(_inkObjects[i]);
+            }
+        }
+    }
+}
